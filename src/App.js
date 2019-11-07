564445c179540143d9013bccc6a4c20eef86e211
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2019-11-07 00:33:44 -0500

    Added antd components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import Button from 'antd/es/button';
 import './App.css';
 
+import { Carousel, Avatar,Timeline } from 'antd';
+
+function onChange(a, b, c){
+    console.log(a, b, c);
+};
+
+
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <Button type="primary">Button</Button>
-        </div>
-    );
-  }
+
+
+    render() {
+        return (
+            <div>
+                <Avatar shape="square" size="large" icon="user" />
+
+                <Carousel autoplay afterChange={onChange}>
+                    <div><h3>1</h3></div>
+                    <div><h3>2</h3></div>
+                    <div><h3>3</h3></div>
+                </Carousel>
+
+                <div>
+                    <Timeline>
+                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                        <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+                        <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+                        <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+                        </Timeline>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
